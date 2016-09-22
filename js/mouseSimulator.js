5cb5fe3a3b78eefc46314239195c0b2ecf72397a
Fabrice Dugas <faberice.dugas@gmail.com>
2016-09-23 00:40:21 +0900

    Moved event simulation into its own file

---
+++ js/mouseSimulator.js
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * File: simulateMouse.js
+ * Desc: functions to simulate mouse events. Event.fake property allows
+ *       handlers to act differently with simulated events (i.e. ignore them)
+ * Author: Fabrice Dugas
+ *****************************************************************************/
+ 
+console.log('mouseSimulator injected');
+ 
+// Create a fake MouseEvent
+function mouseEvent(type, sx, sy, cx, cy) {
+	var e = {
+		bubbles : true,
+		cancelable : (type != "mousemove"),
+		view : window,
+		detail : 0,
+		screenX : sx,
+		screenY : sy,
+		clientX : cx,
+		clientY : cy,
+		button : 0
+	};
+
+	var evt = document.createEvent("MouseEvents");
+	evt.initMouseEvent(type,
+		e.bubbles, e.cancelable, e.view, e.detail,
+		e.screenX, e.screenY, e.clientX, e.clientY,
+		e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
+		e.button, document.body.parentNode);
+
+	evt.button = {
+		0 : 1,
+		1 : 4,
+		2 : 2
+	}
+	[evt.button] || evt.button;
+  
+  evt.fake = true;
+
+	return evt;
+}
