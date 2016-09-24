9758bfcdf5b9f020ec4efb60346fad6e201b4561
Fabrice Dugas <faberice.dugas@gmail.com>
2016-09-24 22:39:14 +0900

    credentials.js: refactor to add OO-ness; server is setup; users can join existing sessions

--- scripts/credentials.js
+++ scripts/credentials.js
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * File: credentials.js
+ * Desc: Credentials page
+ * Author: Fabrice Dugas
+ *****************************************************************************/
+
+/** TODO:
+ * Create Session object
+ * Add createSession and leaveSession
+ * Update UI when stuff...
+ */
 
 // Initialize Firebase
 var config = {
@@ -9,72 +20,78 @@ var config = {
 };
 firebase.initializeApp(config);
 
-/**
- * initApp handles setting up the Firebase context and registering
- * callbacks for the auth status.
- *
- * The core initialization is in firebase.App - this is the glue class
- * which stores configuration. We provide an app name here to allow
- * distinguishing multiple app instances.
- *
- * This method also registers a listener with firebase.auth().onAuthStateChanged.
- * This listener is called when the user is signed in or out, and that
- * is where we update the UI.
- *
- * When signed in, we also authenticate to the Firebase Realtime Database.
- */
-function initApp() {
-  // Listen for auth state changes.
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var head = document.getElementsByTagName('h3')[0];
-      head.style.backgroundImage = "url('../images/pacman64.png')";
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      
-      document.getElementById('sign-in-button').textContent = 'Sign out';
-      document.getElementById('sign-in-status').textContent = 'Signed in';
-    } else {
-      // Let's try to get a Google auth token programmatically.
-      document.getElementById('sign-in-button').textContent = 'Sign-in with Google';
-      document.getElementById('sign-in-status').textContent = 'Signed out';
-    }
-    
-    document.getElementById('sign-in-button').disabled = !isActivated;
-  });
+function Credentials(){
+  // DOM shortcuts
+  this.head = document.getElementsByTagName('h3')[0];
+  this.sessionsList = document.getElementsByClassName('sessions-container')[0];
+  this.activateButton = document.getElementById('activate-button');
+  this.signInButton = document.getElementById('sign-in-button');
+  this.signInStatus = document.getElementById('sign-in-status');
+  
+  this.initFirebase()
   
   // Check if extension was activated by user before activating activate-button
   isActivated(
     function(isActivated) {
-      document.getElementById('activate-button').disabled = isActivated;
-    });
-      
-  document.getElementById('sign-in-button').addEventListener('click', startSignIn, false);
-  document.getElementById('activate-button').addEventListener('click', injectScript, false);
+      this.activateButton.disabled = isActivated;
+    }.bind(this));
+    
+  this.signInButton.addEventListener('click', this.startSignIn.bind(this), false);
+  this.activateButton.addEventListener('click', injectScript, false);
 }
 
-// Asks background.js if the extension was activated by user
-function isActivated(callback) {
-  chrome.runtime.sendMessage({greeting : 'isActivated'}, callback);
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+// Stole from github.com/friendlychat
+Credentials.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+Credentials.ORIGINAL_IM_URL = 'url("../images/pacman64.png")'
+Credentials.LOADING_IMG_URL = 'url("../images/pacman64-loading.gif")'
+
+Credentials.prototype.onAuthStateChanged = function(user) {
+  // Restore original image
+  var headImage = this.head.style.backgroundImage;
+  if (headImage == Credentials.LOADING_IMG_URL){
+    this.head.style.backgroundImage = Credentials.ORIGINAL_IM_URL;
+  }
+  
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    
+    this.signInButton.textContent = 'Sign out';
+    this.signInStatus.textContent = 'Signed in';
+    
+    console.log('User signed in. Loading sessions...')
+    this.loadSessions();
+  } else {
+    // Let's try to get a Google auth token programmatically.
+    this.signInButton.textContent = 'Sign-in with Google';
+    this.signInStatus.textContent = 'Signed out';
+  }
+  
+  this.signInButton.disabled = false;
 }
 
 /**
  * Start the auth flow and authorizes to Firebase.
  * @param{boolean} interactive True if the OAuth flow should request with an interactive mode.
  */
-function startAuth(interactive) {
+Credentials.prototype.startAuth = function(interactive) {
   // Request an OAuth token from the Chrome Identity API.
-  chrome.identity.getAuthToken({interactive: !!interactive}, function(token) {
-    // Change head image when trying to sign in
-    var head = document.getElementsByTagName('h3')[0];
-    head.style.backgroundImage = "url('../images/pacman64-loading.gif')";
-    
+  chrome.identity.getAuthToken({interactive: !!interactive}, function(token) {  
     if (chrome.runtime.lastError && !interactive) {
       console.log('It was not possible to get a token programmatically.');
     } else if(chrome.runtime.lastError) {
@@ -82,31 +99,151 @@ function startAuth(interactive) {
     } else if (token) {
       // Authorize Firebase with the OAuth Access Token.
       var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
-      firebase.auth().signInWithCredential(credential).catch(function(error) {
+      this.auth.signInWithCredential(credential).catch(function(error) {
         // The OAuth token might have been invalidated. Lets' remove it from cache.
         if (error.code === 'auth/invalid-credential') {
           chrome.identity.removeCachedAuthToken({token: token}, function() {
-            startAuth(interactive);
-          });
+            this.startAuth(interactive);
+          }.bind(this));
         }
-      });
+      }.bind(this));
     } else {
       console.error('The OAuth Token was null');
     }
     
-  });
+  }.bind(this));
 }
 
 /**
  * Starts the sign-in process.
  */
-function startSignIn() {
-  document.getElementById('sign-in-button').disabled = true;
-  if (firebase.auth().currentUser) {
-    firebase.auth().signOut();
+Credentials.prototype.startSignIn = function() {
+  this.signInButton.disabled = true;
+  if (this.auth.currentUser) {
+    this.auth.signOut();
   } else {
-    startAuth(true);
+    // Change head image when trying to sign in
+    this.head.style.backgroundImage = Credentials.LOADING_IMG_URL;
+    this.startAuth(true);
   }
+}
+
+Credentials.prototype.loadSessions = function() {
+  console.log('loadSessions called');
+  // Reference to the /sessions/ database path.
+  this.sessionsRef = this.database.ref('sessions');
+  // Make sure we remove all previous listeners.
+  this.sessionsRef.off();
+  
+  // Loads the last 5 sessions and listens for new ones.
+  var setSession = function(data) {
+    console.log('Set session:');
+    console.log(data);
+    var val = data.val();
+    this.displaySession(data.key, val.owner, val.participants);
+  }.bind(this);
+  this.sessionsRef.limitToLast(5).on('child_added', setSession);
+  this.sessionsRef.limitToLast(5).on('child_changed', setSession);
+  
+}
+
+Credentials.SESSION_TEMPLATE =
+    '<div class="session-container">' +
+      '<div class="session-spacing"></div>' +
+      '<div class="session-owner"></div>' +
+      '<div class="session-participants"></div>' +
+      '<button class="session-join-button"></button>' +
+    '</div>';
+
+Credentials.prototype.displaySession = function(key, owner, participants){
+  var div = document.getElementById(key);
+  // If an element for that message does not exists yet we create it.
+  if (!div) {
+    var container = document.createElement('div');
+    container.innerHTML = Credentials.SESSION_TEMPLATE;
+    div = container.firstChild;
+    div.setAttribute('id', key);
+    this.sessionsList.appendChild(div);
+  }
+  div.querySelector('.session-owner').textContent = owner
+  var joinButton = div.querySelector('.session-join-button')
+  joinButton.textContent = 'Join ' + owner
+  
+  // Check if we already joined that session
+  getSession(
+    function(currKey) {
+      joinButton.disabled = (currKey == key);
+    });
+  
+  // Listen to button
+  joinButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.joinSession(key, owner);
+  }.bind(this));
+  
+}
+
+Credentials.prototype.joinSession = function(sessionKey, owner) {
+  // TODO: Check if user is already in a session
+  
+  this.currentSession = sessionKey;
+  var currentUser = this.auth.currentUser;
+  
+  var sessionRef = this.sessionsRef.child(sessionKey);
+  if (sessionRef) {
+    console.log(currentUser.displayName + ' is joining ' + owner);
+    var participantsRef = sessionRef.child('participants');
+    
+    var newParticipantRef = participantsRef.push();
+    var userKey = newParticipantRef.key();
+    
+    newParticipantRef.set({
+      name: currentUser.displayName
+    })
+    .then(function() {
+      var joinButton = document.getElementById(sessionKey).querySelector('#session-join-button')
+      joinButton.disabled = true;
+      saveKeys(sessionKey, userKey);
+    }.bind(this))
+    .catch(function(error) {
+      console.error('Error writing new message to Firebase Database', error);
+    });
+  }
+}
+
+//TODO: leaveSession
+Credentials.prototype.leaveSession = function(sessionKey, userKey) {
+  
+}
+
+//TODO: createSession
+Credentials.prototype.createSession = function(sessionKey, userKey) {
+  
+}
+
+/**
+ * Asks background.js if the extension was activated by user
+ */
+function isActivated(callback) {
+  chrome.runtime.sendMessage({greeting : 'isActivated'}, callback);
+}
+
+/**
+ * Asks background.js for the current session key
+ */
+function getSession(callback) {
+  chrome.runtime.sendMessage({greeting : 'getSession'}, callback);
+}
+
+/**
+ * Save keys in background.js
+ */
+function saveKeys(sessionKey, userKey) {
+  chrome.runtime.sendMessage({
+    greeting : 'saveKeys',
+    sessionKey : sessionKey,
+    userKey : userKey
+    });
 }
 
 /**
@@ -127,5 +264,5 @@ function injectScript() {
  
  
 window.onload = function() {
-  initApp();
+  var credentials = new Credentials();
 };
